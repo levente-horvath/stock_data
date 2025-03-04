eda6f117aff09e795e7026a9a35a7474c4c1992c
Lev Horvath <levente-horvath@outlook.com>
2025-03-04 14:00:17 +0100

    changed to yaho finance, modified extract.py

---
+++ src/extract.py
@@ -0,0 +1,30 @@
+import json
+from utils import setup_logger
+import yfinance as yf
+from datetime import datetime, timedelta
+from utils import setup_logger
+
+
+# Initialize setup logger
+logger = setup_logger(__name__)
+
+
+
+# Returns a the daily stock price up to today
+# Ticker -> Stock symbol
+# Duration -> In days
+def get_stock_price_date(ticker, duration):
+
+    end_date = datetime.today().strftime('%Y-%m-%d')
+    start_date = (datetime.today() - timedelta(days=duration)).strftime('%Y-%m-%d')
+
+    return yf.download(ticker, start=start_date, end=end_date)
+
+
+data = get_stock_price_date("GOOG", 365)
+
+data.to_csv("stockprices.csv")
+
+logger.
+
+
---
+++ src/transform.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from utils import setup_logger
+
+logger = setup_logger(__name__)
+
+
+#daily low/ high prices
+# closing and opening price
+
+# 50 day moving averages
+# 200 day moving averages
+# MACD
+# Trading volume
+
+
