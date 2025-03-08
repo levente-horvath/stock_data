0dcf5be3c9eb8a7c301559acecd5d5be81962deb
Lev Horvath <levente-horvath@outlook.com>
2025-03-09 00:20:45 +0100

    merged extract.py and transform.py

--- src/extract.py
+++ src/extract.py
@@ -3,7 +3,7 @@ from utils import setup_logger
 import yfinance as yf
 from datetime import datetime, timedelta
 from utils import setup_logger
-
+import pandas as pd
 
 # Initialize setup logger
 logger = setup_logger(__name__)
@@ -28,18 +28,35 @@ def get_stock_price_with_specific_date(ticker, start_date, end_date):
     return yf.download(ticker, start=start_date, end=end_date)
 
 
-def main():
+# Cleans the data
+# Returns:
+# ticker -> name of the ticker : string
+# df -> the stock prices : data frame
+def transform_data(df):
 
+    df.reset_index(inplace=True)
+    df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+    
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    return df
+
+
+
+def main():
     data = get_stock_price_with_specific_date("ANET", "2025-3-1", "2025-3-6")
 
-    print(data)
+    tr_data = transform_data(data)
+    tr_data.to_json('test_data.json')
+
+
+"""
 
     filename = "stockprices.csv"
-    data.to_csv(filename)
-
+    tr_data.to_csv(filename)
     logger.info(f"Data saved to {filename}")
 
-
+"""
 
 
 if __name__ == "__main__":
